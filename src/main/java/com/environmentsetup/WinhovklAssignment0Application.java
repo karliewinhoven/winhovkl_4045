cdeac1c329260c2d7fb30d09f83fe82b2e64420f
Administrator <karliewinhoven@gmail.com>
2019-09-27 19:31:59 -0400

    Final Commit

---
+++ src/main/java/com/environmentsetup/WinhovklAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.environmentsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinhovklAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinhovklAssignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/environmentsetup/winhovkl_Assignment0/WinhovklAssignment0ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.environmentsetup.winhovkl_Assignment0;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WinhovklAssignment0ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
